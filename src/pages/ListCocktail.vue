7fe92012249bd9de9cfe50ef2f6cef329b12c3fe
Gustavo-sbw <grojas@safebywolf.cl>
2026-09-16 01:32:13 -0500

    feat(orden según numero de ingredientes)
    Se ordena la lista según el numero de ingredientes. Los coctail que tienen menos ingredientes van al principio

--- src/pages/ListCocktail.vue
+++ src/pages/ListCocktail.vue
@@ -268,21 +268,17 @@ const doFilter = (filterType) => {
     cocktailList.value.sort((a,b) =>  a.name.localeCompare(b.name));
   }
   else if (filterType === 'ingredients'){
-     cocktailList.value = cocktailList.value.filter(cocktail =>
+    cocktailList.value = cocktailList.value.filter(cocktail =>
       searchIngredientsList.value.every(ingredientToSearch =>
         cocktail.ingredients.some(ingredient =>
           ingredient.name.toLowerCase().includes(ingredientToSearch.toLowerCase())
-        )
-      )
-    );
+    )));
   }
   else if (filterType === 'numberIngredients'){
-
+    cocktailList.value.sort((a, b) => a.ingredients.length - b.ingredients.length);
   }
   else if (filterType === 'difficulty'){
-    cocktailList.value.sort((a, b) =>
-      difficultyOrder[a.difficulty] - difficultyOrder[b.difficulty]
-    )
+    cocktailList.value.sort((a, b) => difficultyOrder[a.difficulty] - difficultyOrder[b.difficulty]);
   }
 }
 
